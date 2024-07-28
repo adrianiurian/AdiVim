4f3d278685ce2abf0d338b1535571e80409efb15
Adrian Iurian <adrian.iurian1@gmail.com>
2024-07-28 04:24:03 +0300

    added colors, bars and comments

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,9 +1,30 @@
 return {
-	"terrortylor/nvim-comment",
-	config = function()
-		require("nvim_comment").setup()
+	{
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup()
 
-		vim.keymap.set({ "n", "v" }, "<leader>/", " :CommentToggle<CR>")
-	end,
+			vim.keymap.set({ "n", "v" }, "<leader>/", " :CommentToggle<CR>")
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			signs = true,
+			keywords = {
+				FIX = {
+					icon = " ", -- icon used for the sign, and in search results
+					color = "error", -- can be a hex color, or a named color (see below)
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+				},
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+		},
+	},
 }
-
